9a7f071f1d2be9e8ddcbf916cb4f5f9c2972054d
Maia Kaplan <maia@streamlinegenomics.com>
2019-12-10 07:17:14 -0500

    Add superclass and abstract method

--- shapes.py
+++ shapes.py
@@ -10,9 +10,16 @@ class Point():
     def distance(self, other_point):
         return sqrt((self.x - other_point.x)**2 + (self.y - other_point.y)**2)
 
+from abc import ABC, abstractmethod
+## 8. abstract class with abstract contains method
+class Shape(ABC):
+    @abstractmethod
+    def contains():
+        pass
+
 
 ## 3. circle
-class Circle():
+class Circle(Shape):
     def __init__(self, point, r):
         self.center = point
         self.radius = r
@@ -28,7 +35,7 @@ class Circle():
             return False
 
 ## 5. rectangle
-class Rectangle():
+class Rectangle(Shape):
     def __init__(self, bottomleft, width, height):
         self.bottomleft = bottomleft
         self.width = width
@@ -55,5 +62,14 @@ class Square(Rectangle):
         self.width = side
         self.height = side
 
+    
+## 9. compound shape with contains being the union of pass shapes
+class CompoundShape(Shape):
+    def __init__(self, list_of_shapes):
+        self.list_of_shapes = list_of_shapes
 
-
+    def contains(self, point):
+        for each in self.list_of_shapes:
+            if each.contains(point):
+                return True
+        return False
